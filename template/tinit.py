efe28e01ab8e7a2df9462f40e4ed3054fbbe4e51
gen740 <genfujimoto@gmail.com>
2022-08-31 19:26:25 +0900

    Update

--- template/tinit.py
+++ template/tinit.py
@@ -7,13 +7,38 @@ import toml
 config: Dict[str, Any]
 
 
+def edit(name: str):
+    """
+    edit the template files
+    """
+    global config
+    files = ""
+    for i in config["template"]:
+        if i["name"] == name:
+            for j in i["files"]:
+                files += os.path.join(config["TemplatePath"], j) + " "
+            break
+    else:
+        print("There in no match template: ", name)
+        sys.exit(1)
+    print("will edit ", files)
+    yn = input("Y/n: ")
+    if yn in ["Y", "y", "yes", "Yes", "YES"]:
+        os.system("nvim " + files)
+    else:
+        sys.exit(0)
+
+
 def tinit(name: str):
     global config
     for i in config["template"]:
         if i["name"] == name:
-            # shutil.copy("")
-            os.path.join(config["TemplatePath"])
-            print(config["Path"])
+            for j in i["files"]:
+                shutil.copy(os.path.join(config["TemplatePath"], j), ".")
+            break
+    else:
+        print("There in no match template: ", name)
+        sys.exit(1)
 
 
 def run():
@@ -34,10 +59,20 @@ def run():
             description = ""
             try:
                 description = i["description"]
-            except Exception as e:
-                raise e
+            except Exception as _:
+                pass
             print("\t--", i["name"], "\t", description)
         sys.exit(0)
+    if arg1 == "--edit" or arg1 == "-e":
+        arg2 = ""
+        try:
+            arg2 = sys.argv[2]
+        except Exception as _:
+            print("Usage:")
+            print("\t --edit [templatename]")
+            sys.exit(0)
+        edit(arg2)
+
     else:
         tinit(arg1)
-    print("Tinit Done")
+    print("==================== Tinit Done ====================")
